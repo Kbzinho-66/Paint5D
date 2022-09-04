3ddf32ad7bc8188c0b0f463fda5f52119e11d592
Kbzinho-66 <gvalles@ucs.br>
2022-09-03 21:38:02 -0300

    Organização geral

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,6 @@
 #include <sstream>
 #include <unistd.h>
 
-using namespace std;
-
 std::string FILENAME ="marvel-wallpaper.bmp";
 std::string SAIDA ="saida.bmp";
 
@@ -30,9 +28,10 @@ const int VERMELHO = 0;
 const int VERDE = 0;
 const int AZUL = 0;
 
+/******************** FUNÇÕES BÁSICAS ********************/
+
 // Gera uma estrutura Point a partir de valores para x e y
-Point getPoint(int x, int y)
-{
+Point getPoint(int x, int y) {
     Point p;
     p.x = x;
     p.y = y;
@@ -40,66 +39,73 @@ Point getPoint(int x, int y)
 }
 
 // Obtém a cor de um pixel de uma determinada posição
-Uint32 getPixel(int x, int y)
-{
+Uint32 getPixel(int x, int y) {
     if((x>=0 && x<=width) && (y>=0 && y<=height))
         return pixels[x + width * y];
     else
         return -1;
 }
 
-// seta um pixel em uma determinada posição,
-// através da coordenadas de cor r, g, b, e alpha (transparência)
+// Seta um pixel em uma determinada posição, através da coordenadas de cor r, g, b, e alpha (transparência)
 // r, g, b e a variam de 0 até 255
-void setPixel(int x, int y, int r, int g, int b, int a)
-{
+void setPixel(int x, int y, int r, int g, int b, int a) {
     pixels[x + y * width] = SDL_MapRGBA(window_surface->format, r, g, b, a);
 }
 
-// seta um pixel em uma determinada posição,
-// através da coordenadas de cor r, g e b
+// Seta um pixel em uma determinada posição, através da coordenadas de cor r, g e b
 // r, g, e b variam de 0 até 255
-void setPixel(int x, int y, int r, int g, int b)
-{
+void setPixel(int x, int y, int r, int g, int b) {
     setPixel(x, y, r, g, b, 255);
 }
 
-// Mostra na barra de título da janela a posição
-// corrente do mouse
-void showMousePosition(SDL_Window * window, int x, int y)
-{
+// Seta um pixel em uma determinada posição através das coordenadas de cor r, g e b
+// r, g, e b variam de 0 até 255
+void setPixel(Point p, int r, int g, int b) {
+    setPixel(p.x, p.y, r, g, b, 255);
+}
+
+// Seta um pixel em uma determinada posição, através de um Uint32 representando uma cor RGB
+void setPixel(int x, int y, Uint32 color) {
+    if((x<0 || x>=width || y<0 || y>=height)) {
+        return;
+    }
+    pixels[x + y * width] = color;
+}
+
+// Seta um pixel em uma determinada posição,
+// através de um Uint32 representando
+// uma cor RGB
+void setPixel(Point p, Uint32 color) {
+    int x, y;
+    x = p.x;
+    y = p.y;
+
+    if((x<0 || x>=width || y<0 || y>=height)) {
+        return;
+    }
+    pixels[x + y * width] = color;
+}
+
+// Mostra na barra de título da janela a posição atual do mouse
+void showMousePosition(SDL_Window * window, int x, int y) {
     std::stringstream ss;
     ss << titulo << " X: " << x << " Y: " << y;
     SDL_SetWindowTitle(window, ss.str().c_str());
 }
 
-// Imprime na console a posição corrente do mouse
-void printMousePosition(int x, int y)
-{
+// Imprime na console a posição atual do mouse
+void printMousePosition(int x, int y) {
     printf("Mouse on x = %d, y = %d\n",x,y);
 }
 
-// seta um pixel em uma determinada posição,
-// através de um Uint32 representando
-// uma cor RGB
-void setPixel(int x, int y, Uint32 color)
-{
-    if((x>=0 && x<width && y>=0 && y<height)) {
-        pixels[x + y * width] = color;
-    }
-}
-
-// retorna uma cor RGB(UInt32) formada
-// pelas componentes r, g, b e a(transparência)
-// informadas. r, g, b e a variam de 0 até 255
+// retorna uma cor RGB(UInt32) formada pelas componentes r, g, b e a(transparência) informadas. 
+// r, g, b e a variam de 0 até 255
 Uint32 RGB(int r, int g, int b, int a) {
     return SDL_MapRGBA(window_surface->format, r, g, b, a);
 }
 
-// retorna uma cor RGB(UInt32) formada
-// pelas componentes r, g, e b
-// informadas. r, g e b variam de 0 até 255
-// a transparência é sempre 255 (imagem opaca)
+// retorna uma cor RGB(UInt32) formada pelas componentes r, g, e b informadas. 
+// r, g e b variam de 0 até 255, a transparência é sempre 255 (imagem opaca)
 Uint32 RGB(int r, int g, int b) {
     return SDL_MapRGBA(window_surface->format, r, g, b, 255);
 }
@@ -132,112 +138,6 @@ Uint8 getColorComponent( Uint32 pixel, char component ) {
     return (Uint8) pixel;
 }
 
-void bresenham(int x1, int y1, int x2, int y2, Uint32 cor)
-{
-    int x,y,dx,dy,dx1,dy1,px,py,xe,ye,i;
-
-    dx=x2-x1;
-    dy=y2-y1;
-
-    dx1=fabs(dx);
-    dy1=fabs(dy);
-
-    px=2*dy1-dx1;
-    py=2*dx1-dy1;
-
-    if(dy1<=dx1)
-    {
-        if(dx>=0)
-        {
-            x=x1;
-            y=y1;
-            xe=x2;
-        }
-        else
-        {
-            x=x2;
-            y=y2;
-            xe=x1;
-        }
-        setPixel(x,y,cor);
-        for(i=0;x<xe;i++)
-        {
-            x=x+1;
-            if(px<0)
-            {
-                px=px+2*dy1;
-            }
-            else
-            {
-                if((dx<0 && dy<0) || (dx>0 && dy>0))
-                {
-                    y=y+1;
-                }
-                else
-                {
-                    y=y-1;
-                }
-                px=px+2*(dy1-dx1);
-            }
-            setPixel(x,y,cor);
-        }
-    }
-    else
-    {
-        if(dy>=0)
-        {
-            x=x1;
-            y=y1;
-            ye=y2;
-        }
-        else
-        {
-            x=x2;
-            y=y2;
-            ye=y1;
-        }
-        setPixel(x,y,cor);
-        for(i=0;y<ye;i++)
-        {
-            y=y+1;
-            if(py<=0)
-            {
-                py=py+2*dx1;
-            }
-            else
-            {
-                if((dx<0 && dy<0) || (dx>0 && dy>0))
-                {
-                    x=x+1;
-                }
-                else
-                {
-                    x=x-1;
-                }
-                py=py+2*(dx1-dy1);
-            }
-            setPixel(x,y,cor);
-        }
-    }
-}
-
-void quadriculado(int distancia, Uint32 cor)
-{
-    for (int x = 0; x < width; x++)
-    {
-        if(x % distancia == 0) {
-            bresenham(x,0,x,height-1,cor);
-        }
-    }
-
-    for (int y = 0; y < height; y++)
-    {
-        if(y % distancia == 0) {
-            bresenham(0,y,width-1,y,cor);
-        }
-    }
-}
-
 int iPartOfNumber(float x) {
     return (int) x;
 }
@@ -250,18 +150,8 @@ float rfPartOfNumber(float x) {
     return 1 - fPartOfNumber(x);
 }
 
-Uint32 getRandomColor() {
-
-    int r, g, b;
-    r = rand() % 255;
-    g = rand() % 255;
-    b = rand() % 255;
-
-    return RGB(r, g, b);
-}
-
+// Desenha todas as 8 coordenadas de (x, y), uma para cada octante
 void displayBresenhamCircle(int xc, int yc, int x, int y, Uint32 color) {
-    // Desenha todas as 8 coordenadas de (x, y), uma para cada octante
 
     setPixel(xc+x, yc+y, color);
     setPixel(xc-x, yc+y, color);
@@ -274,41 +164,16 @@ void displayBresenhamCircle(int xc, int yc, int x, int y, Uint32 color) {
 
 }
 
-void bresenhamCircle(int xc, int yc, int radius, Uint32 color) {
-    int x = 0;
-    int y = radius;
-    int decisionParameter = 3 - 2 * radius;
+/******************** FUNÇÕES DE DESENHO ********************/
 
-    displayBresenhamCircle(xc, yc, x, y, color);
-
-    while (y >= x) {
-        x++;
-
-        if (decisionParameter > 0) {
-            y--;
-            decisionParameter = decisionParameter + 4 * (x - y) + 10;
-        } else {
-            decisionParameter = decisionParameter + 4 * x + 6;
-        }
-
-        displayBresenhamCircle(xc, yc, x, y, color);
-    }
-
-}
-
-void bresenhamCircle(Point p, int radius, Uint32 color) {
-    int xc = p.x;
-    int yc = p.y;
-    bresenhamCircle(xc, yc, radius, color);
-}
-
+// Preenche uma região com uma cor partindo de um ponto (x,y) enquanto houver pixels da cor original
 void floodFill(int x, int y, Uint32 newColor, Uint32 oldColor) {
 
     if (x < 0 || x > width - 1 || y < 0 || y > height - 1) {
         return;
     }
 
-    stack<Point> st;
+    std::stack<Point> st;
     st.push(getPoint(x, y));
 
     while (st.size() > 0) {
@@ -331,6 +196,12 @@ void floodFill(int x, int y, Uint32 newColor, Uint32 oldColor) {
     }
 }
 
+void floodFill(int x, int y, Uint32 color) {
+    Uint32 oldColor = getPixel(x, y);
+    floodFill(x, y, color, oldColor);
+}
+
+// Preenche uma região com uma cor partindo de um ponto enquanto houver pixels da cor original
 void floodFill(Point p, Uint32 newColor, Uint32 oldColor) {
     int x, y;
     x = p.x;
@@ -338,6 +209,129 @@ void floodFill(Point p, Uint32 newColor, Uint32 oldColor) {
     floodFill(x, y, newColor, oldColor);
 }
 
+// Preenche uma região indefinida com uma cor partindo de um ponto
+void floodFill(Point p, Uint32 color) {
+    Uint32 oldColor = getPixel(p.x, p.y);
+    floodFill(p, color, oldColor);
+}
+
+// Desenha uma linha de Bresenham a partir de 4 coordenadas e uma cor
+void bresenham(int x1, int y1, int x2, int y2, Uint32 cor) {
+    int x,y,dx,dy,dx1,dy1,px,py,xe,ye,i;
+
+    dx=x2-x1;
+    dy=y2-y1;
+
+    dx1=fabs(dx);
+    dy1=fabs(dy);
+
+    px=2*dy1-dx1;
+    py=2*dx1-dy1;
+
+    if(dy1<=dx1) {
+        if(dx>=0) {
+            x=x1;
+            y=y1;
+            xe=x2;
+        } else {
+            x=x2;
+            y=y2;
+            xe=x1;
+        }
+        setPixel(x,y,cor);
+        for(i=0;x<xe;i++) {
+            x=x+1;
+            if(px<0) {
+                px=px+2*dy1;
+            } else {
+                if((dx<0 && dy<0) || (dx>0 && dy>0)) {
+                    y=y+1;
+                } else {
+                    y=y-1;
+                }
+                px=px+2*(dy1-dx1);
+            }
+            setPixel(x,y,cor);
+        }
+    } else {
+        if(dy>=0) {
+            x=x1;
+            y=y1;
+            ye=y2;
+        } else {
+            x=x2;
+            y=y2;
+            ye=y1;
+        }
+        setPixel(x,y,cor);
+        for(i=0;y<ye;i++) {
+            y=y+1;
+            if(py<=0) {
+                py=py+2*dx1;
+            } else {
+                if((dx<0 && dy<0) || (dx>0 && dy>0)) {
+                    x=x+1;
+                } else {
+                    x=x-1;
+                }
+                py=py+2*(dx1-dy1);
+            }
+            setPixel(x,y,cor);
+        }
+    }
+}
+
+// Desenha uma linha de Bresenham com base em 2 pontos e uma cor
+void bresenham(Point p1, Point p2, Uint32 color) {
+    bresenham(p1.x, p1.y, p2.x, p2.y, color);
+}
+
+// Desenha um retângulo com base no ponto superior esquerdo, no inferior direito e em uma cor
+void rectangle(Point p1, Point p2, Uint32 color, bool fill = false) {
+    bresenham(p1.x, p1.y, p2.x, p1.y, color);
+    bresenham(p1.x, p1.y, p1.x, p2.y, color);
+    bresenham(p1.x, p2.y, p2.x, p2.y, color);
+    bresenham(p2.x, p1.y, p2.x, p2.y, color);
+
+    if (fill) {
+        floodFill(p1.x + 1, p1.y + 1, color);
+    }
+}
+
+// Desenha um círculo de Bresenham com base em duas coordenadas para o ponto central,
+// um valor para o raio e uma cor
+void bresenhamCircle(int xc, int yc, int radius, Uint32 color) {
+    int x = 0;
+    int y = radius;
+    int decisionParameter = 3 - 2 * radius;
+
+    displayBresenhamCircle(xc, yc, x, y, color);
+
+    while (y >= x) {
+        x++;
+
+        if (decisionParameter > 0) {
+            y--;
+            decisionParameter = decisionParameter + 4 * (x - y) + 10;
+        } else {
+            decisionParameter = decisionParameter + 4 * x + 6;
+        }
+
+        displayBresenhamCircle(xc, yc, x, y, color);
+    }
+
+}
+
+// Desenha um círculo de Bresenham com base em um Ponto central, um raio e uma cor.
+void bresenhamCircle(Point p, int radius, Uint32 color) {
+    int xc = p.x;
+    int yc = p.y;
+    bresenhamCircle(xc, yc, radius, color);
+}
+
+// Desenha uma curva de Bézier com base em 4 pontos e uma cor.
+// p[0] e p[3] são os pontos extremos da curva, pelos quais a curva vai passar
+// p[1] e p[2] são pontos de controle, que puxam a curva, mas não fazem parte dela
 void bezierCurve(Point p[4], Uint32 color) {
     double xu, yu, esp;
 
@@ -349,22 +343,8 @@ void bezierCurve(Point p[4], Uint32 color) {
     }
 }
 
-void rectangle(Point p1, Point p2, Uint32 color, bool fill) {
-    bresenham(p1.x, p1.y, p2.x, p1.y, color);
-    bresenham(p1.x, p1.y, p1.x, p2.y, color);
-    bresenham(p1.x, p2.y, p2.x, p2.y, color);
-    bresenham(p2.x, p1.y, p2.x, p2.y, color);
-
-    if (fill) {
-        for (int y = p1.y; y < p2.y; y++) {
-            bresenham(p1.x, y, p2.x, y, color);
-        }
-    }
-}
-
 // Aqui ocorrem as chamadas das funções a ser exibidas na janela
-void display()
-{
+void display() {
     Point p1, p2;
     p1 = getPoint(0, 599 - 78);
     p2 = getPoint(799, 599);
@@ -374,12 +354,9 @@ void display()
     rectangle(p1, p2, color, true);
 }
 
-void reset_screen()
-{
-    for (int y = 0; y < height; ++y)
-    {
-        for (int x = 0; x < width; ++x)
-        {
+void reset_screen() {
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
             setPixel(x, y, RGB(VERMELHO,VERDE,AZUL));
         }
     }
@@ -388,8 +365,7 @@ void reset_screen()
 //void copia_para_bmp(Bit)
 // Inicializa o SDL, abre a janela e controla o loop
 // principal do controle de eventos
-int main()
-{
+int main() {
     // Inicializações iniciais obrigatórias
 
     int result;
@@ -402,10 +378,12 @@ int main()
 
     if(imagem) {
 
-        SDL_Window * window = SDL_CreateWindow(titulo.c_str(),
+        SDL_Window * window = SDL_CreateWindow(
+            titulo.c_str(),
             SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
             imagem->w, imagem->h,
-            SDL_WINDOW_RESIZABLE);
+            SDL_WINDOW_RESIZABLE
+        );
 
         window_surface = SDL_GetWindowSurface(window);
 
@@ -420,15 +398,12 @@ int main()
         printf("SDL Pixel format: %s\n",
             SDL_GetPixelFormatName(window_surface->format->format));
 
-        while (1)
-        {
+        while (true) {
 
             SDL_Event event;
 
-            while (SDL_PollEvent(&event))
-            {
-                if (event.type == SDL_QUIT)
-                {
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_QUIT) {
                     exit(0);
                 }
 
@@ -446,16 +421,12 @@ int main()
                             break;
 
                         case SDLK_q:
-
-                            quadriculado(20, RGB(255, 0, 0));
                             break;
                         }
                 }
 
-                if (event.type == SDL_WINDOWEVENT)
-                {
-                    if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
-                    {
+                if (event.type == SDL_WINDOWEVENT) {
+                    if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
                         window_surface = SDL_GetWindowSurface(window);
                         pixels = (unsigned int *) window_surface->pixels;
                         width = window_surface->w;
@@ -466,16 +437,14 @@ int main()
                 }
 
                 // Se o mouse é movimentado
-                if(event.type == SDL_MOUSEMOTION)
-                {
+                if(event.type == SDL_MOUSEMOTION) {
                     // Mostra as posições x e y do mouse
                     showMousePosition(window,event.motion.x,event.motion.y);
                 }
-                if(event.type == SDL_MOUSEBUTTONDOWN)
-                {
+
+                if(event.type == SDL_MOUSEBUTTONDOWN) {
                     /*Se o botão esquerdo do mouse é pressionado */
-                    if(event.button.button == SDL_BUTTON_LEFT)
-                    {
+                    if(event.button.button == SDL_BUTTON_LEFT) {
                         printf("Mouse pressed on (%d,%d)\n",event.motion.x,event.motion.y) ;
                     }
                 }
@@ -483,14 +452,16 @@ int main()
 
             // Seta a cor de fundo da janela para a informada nas
             // constantes VERMELHO, VERDE e AZUL
-            //reset_screen();
+            reset_screen();
 
-            //mostra_bmp(bmp);
+            // mostra_bmp(bmp);
 
             display();
 
             SDL_UpdateWindowSurface(window);
         }
 
+    } else {
+        printf("Sem imagem a carregar.\n");
     }
 }
